110f53b5ebdc58f64d2944a67ebe74fd78c2db25
X3N0V3RS3 <31805668+X3N0V3RS3@users.noreply.github.com>
2022-10-01 21:35:28 -0400

    Update autoanonsurf.py

--- autoanonsurf.py
+++ autoanonsurf.py
@@ -14,13 +14,13 @@ os.system("clear")
 i = 0
 
 def ipchange():
- os.system("anonsurf myip")
- time.sleep(2.5)  
- os.system("anonsurf change")
- time.sleep(2.5)  
- os.system("anonsurf myip") 
- time.sleep(2.5)  
- os.system("clear") 
+   os.system("anonsurf myip")
+   time.sleep(2.5)  
+   os.system("anonsurf change")
+   time.sleep(2.5)  
+   os.system("anonsurf myip") 
+   time.sleep(2.5)  
+   os.system("clear") 
 
 while i == 0:
    time.sleep(timetonodechange)
